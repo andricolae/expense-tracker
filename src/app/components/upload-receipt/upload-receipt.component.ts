59eac2b05710c1bacfde995190450f90ed03e382
francesco.maxim <francesco.maxim@icloud.com>
2025-02-21 18:15:31 +0200

    init upload receipt component

---
+++ src/app/components/upload-receipt/upload-receipt.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-receipt',
+  imports: [],
+  templateUrl: './upload-receipt.component.html',
+  styleUrl: './upload-receipt.component.css'
+})
+export class UploadReceiptComponent {
+
+}
