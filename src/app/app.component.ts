e18a3fec8ba05bfc669f1db84f1e7a3541133834
francesco.maxim <francesco.maxim@icloud.com>
2025-02-21 18:27:57 +0200

    v1 for extracting text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CommonModule } from '@angular/common';
-
+import { UploadReceiptComponent } from './components/upload-receipt/upload-receipt.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, CommonModule],  
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    CommonModule,
+    UploadReceiptComponent,
+  ],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
